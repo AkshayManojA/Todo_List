93b3af348e3c341c081b67da51d21dbae7f0f3f1
AkshayManojA <akshaymanojanadakkal@gmail.com>
2024-09-30 11:04:03 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+// login in the Todo list page
+function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === 'admin' && password === '12345') {
+        document.getElementById('loginPage').style.display = 'none';
+        document.getElementById('todoPage').style.display = 'block';
+    } else {
+        alert('Invalid Username and Password!');
+    }
+}
+// logout 
+
+function logout() {
+    document.getElementById('username').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('loginPage').style.display = 'block';
+    document.getElementById('todoPage').style.display = 'none';
+    document.getElementById('todoBody').innerHTML = '';
+}
+
+function fetchTodos() {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(data => {
+            const todoBody = document.getElementById('todoBody');
+            todoBody.innerHTML = '';
+
+            data.forEach(todo => {
+                const row = document.createElement('tr');
+
+                // Create table cell
+
+                const idCell = document.createElement('td');
+                idCell.textContent = todo.id;
+                const titleCell = document.createElement('td');
+                titleCell.textContent = todo.title;
+                const completedCell = document.createElement('td');
+                
+                // Creating  checkbox
+
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.checked = todo.completed;
+
+                // To mark finish tasks
+
+                if (!todo.completed) {
+                    checkbox.addEventListener('click', () => {
+                        todo.completed = checkbox.checked;
+                   
+                    });
+                } else {
+                    checkbox.disabled = true;
+                }
+
+                completedCell.appendChild(checkbox);
+                row.appendChild(idCell);
+                row.appendChild(titleCell);
+                row.appendChild(completedCell);
+
+                todoBody.appendChild(row);
+            });
+        })
+        .catch(error => console.error('Error fetching todos:', error));
+}
